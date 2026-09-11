83604da89d03fa4e484ee475c5383fbbdadd547f
sandulmk <sandulmk194@gmail.com>
2026-09-11 10:37:28 +0530

    Add files via upload

---
+++ co1/preferences/pref/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.pref;
+
+import android.os.Bundle;
+
+import android.content.Intent;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button b;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        b = findViewById(R.id.button);
+    }
+    public void Click(View view){
+        Intent i = new Intent(this,MainActivity2.class);
+        i.putExtra("name", "Sandul");
+        i.putExtra("name1", "M K");
+        startActivity(i);
+
+    }
+}
---
+++ co1/preferences/pref/MainActivity2.java
@@ -0,0 +1,21 @@
+package com.example.pref;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity2 extends AppCompatActivity {
+    TextView a1, a2;
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        a1 = findViewById(R.id.n1);
+        a2 = findViewById(R.id.n2);
+        Bundle extra = getIntent().getExtras();
+        String text = extra.getString("name");
+        String text1 = extra.getString("name1");
+        a1.setText(text);
+        a2.setText(text1);
+    }
+}
